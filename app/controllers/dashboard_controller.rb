17b31ed2c0a52a017c838686660e0b080dfd37b4
MohamedZainFayed <mohamed.z.fayed@gmail.com>
2018-06-17 17:26:42 +0200

    fix dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class DashboardController < ApplicationController
 
     def index
-        respond_to do |format|
-            format.all do
-                render json: User.all.as_json
-            end
-        end
+        
     end
 end
